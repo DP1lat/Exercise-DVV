3be49e068813c78ec41177b1e7cd2a1c4fca4f73
DP1lat <dpilat94@gmail.com>
2025-03-11 09:12:24 -0300

    Calculadora

---
+++ Calculadora/scripts.js
@@ -0,0 +1,11 @@
+const buttons = document.querySelectorAll(".number");
+const totalDisplay = document.getElementById("calc");
+let total = 0;
+
+buttons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        total = join[]
+        totalDisplay.textContent = total; // Atualiza o total na tela
+    });
+});
+
