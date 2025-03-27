b63ab9b889a763fab4116db85f55b40c3304dbc5
DP1lat <dpilat94@gmail.com>
2025-03-26 22:51:08 -0300

    api advice
    adicionado a api e funcionalidade dos botões, atualização do texto e do numero do conselho

--- API-conselhos/scripts.js
+++ API-conselhos/scripts.js
@@ -1,11 +1,30 @@
 const diceBtn = document.querySelector(".dice-btn");
+const BASE_URL = "https://api.adviceslip.com/advice";
+const adviceNumber = document.getElementById("advice-number");
+const adviceText = document.getElementById("advice-text");
 
-diceBtn.addEventListener("click", () => {
+diceBtn.addEventListener("click", async () => {
     diceBtn.style.animation = "girar .7s ease";
     diceBtn.style.backgroundColor = "white";
+
+    await showAdvice();
 
     setTimeout(() => {
         diceBtn.style.animation = "";
         diceBtn.style.backgroundColor = "#FBCE18";
     }, 600);
-})
+});
+
+async function getAdvice() {
+    const response = await fetch(BASE_URL)
+    const data = await response.json()
+    return data.slip
+}
+
+async function showAdvice() {
+    const advice = await getAdvice();
+    adviceNumber.textContent = "#" + advice.id;
+    adviceText.textContent = advice.advice;
+}
+
+showAdvice();
