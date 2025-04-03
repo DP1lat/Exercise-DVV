dfadf63e34c76ca3e28f8b3a054ec1c3ed723651
DP1lat <dpilat94@gmail.com>
2025-04-02 22:19:45 -0300

    adicionado a api
    criação da logica da api para retirar o cep com o endereço

--- Formulario-de-Endereco/script.js
+++ Formulario-de-Endereco/script.js
@@ -1,20 +1,31 @@
 const submitBtn = document.getElementById("submit-btn");
-const cep = document.getElementById("cep");
+const cepInput = document.getElementById("cep");
+const streetInput = document.getElementById("street");
+const neighborhoodInput = document.getElementById("neighborhood");
+const cityInput = document.getElementById("city");
+const stateInput = document.getElementById("state");
 
-const BASE_URL = "https://viacep.com.br/ws/01001000/json/"
+const BASE_URL = "https://viacep.com.br/ws/"
 
 submitBtn.addEventListener("click", (e) => {
     e.preventDefault();
 
-    console.log("Cliquei")
+    const cepValor = cepInput.value;
+
+    console.log(cepValor)
+
+    const url = `${BASE_URL}${cepValor}/json/`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            streetInput.value = data.logradouro;
+            neighborhoodInput.value = data.bairro;
+            cityInput.value = data.localidade;
+            stateInput.value = data.uf;
+        })
+        .catch(error => {
+            console.error("Erro ao buscar o CEP:", error);
+        });
 })
-
-
-
-async function getAdress() {
-    const response = await fetch(BASE_URL)
-    const data = await response.json()
-    console.log(data)
-}
-
-getAdress()
